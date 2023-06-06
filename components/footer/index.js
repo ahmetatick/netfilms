86b4a6e2afcdfe6308e21e28f4301bcd6e2c85f2
ahmet <mdahmetatik@gmail.com>
2023-06-05 22:58:33 -0400

    june5-v1

---
+++ components/footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      Made with 🖤 by&nbsp;
+      <Link
+        href="https://www.linkedin.com/in/ahmetatik"
+        target="_blank"
+      >
+        Ahmet Atik
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
