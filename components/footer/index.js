ed7a8d5bd5ea6683f493155465e31371e846992c
ahmet <mdahmetatik@gmail.com>
2023-06-07 23:52:33 -0400

    june7-v4

--- components/footer/index.js
+++ components/footer/index.js
@@ -10,7 +10,7 @@ const Footer = () => {
         href="https://www.linkedin.com/in/ahmetatik"
         target="_blank"
       >
-        Ahmet Ati
+        Ahmet Atik
       </Link>
     </footer>
   );
