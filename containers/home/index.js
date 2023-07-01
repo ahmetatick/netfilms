314614692c8c8976efc930147dbc8781db3fd254
ahmet <mdahmetatik@gmail.com>
2023-06-30 23:53:59 -0400

    june30-v4

--- containers/home/index.js
+++ containers/home/index.js
@@ -18,9 +18,6 @@ const HomeContainer = () => {
         title="Your Favourites"
         movies={Movies.results.slice(8, 16)}
       />
-      <br />
-      <br />
-      <br />
     </div>
   );
 };
