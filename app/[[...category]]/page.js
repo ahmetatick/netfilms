f24e1664a3d08dc2e4bab16cce6403259a49b306
ahmet <mdahmetatik@gmail.com>
2023-07-07 10:30:55 -0400

    july7-v4

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -8,9 +8,6 @@ async function delay(ms) {
 
 const HomePage = async ({ params }) => {
   await delay(20000);
-  await delay(20000);
-  await delay(20000);
-  await delay(20000);
   let selectedCategory;
   if (params.category?.length > 0) {
     selectedCategory = true;
